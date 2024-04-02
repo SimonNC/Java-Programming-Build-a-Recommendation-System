7587ad69e996a6720ac07aeb13b6a99d3d371974
SimonNC <simon.jorite@gmail.com>
2024-04-02 15:30:18 +1100

    Week3

---
+++ Week3/Assignment 1 - Efficiency/Rater.java
@@ -0,0 +1,22 @@
+/**
+ * Rater is an interface for PlainRater and EfficientRater classes.
+ *
+ * @ Simon JORITE
+ * @ Version: 1.0
+ */
+
+import java.util.*;
+
+public interface Rater {
+  public void addRating(String item, double rating);
+
+  public boolean hasRating(String item);
+
+  public String getID();
+
+  public double getRating(String item);
+
+  public int numRatings();
+
+  public ArrayList<String> getItemsRated();
+}
